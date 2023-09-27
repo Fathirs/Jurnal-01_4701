a5759d09d4e08a6e81b1474b42977b0f574c8f6e
Muhammad Fathir Ramadhennis <103559453+Fathirs@users.noreply.github.com>
2023-09-27 14:03:21 +0700

    Create Jurnal01.java

---
+++ Jurnal01.java
@@ -0,0 +1,13 @@
+public class Jurnal01  {
+    public static void main(String[] args) {
+        String myName = "M Fathir Ramadhennis";
+        String myNim = "607062330014";
+        String myClass = "01" ;
+    
+        System.out.println("Nama: " +myName);
+        System.out.println("NIM: " +myNim);
+        System.out.println("Kelas: " +myClass);
+    }
+  
+
+}
